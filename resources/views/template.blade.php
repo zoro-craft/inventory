3923ece2c31781cc818c18d214ae0cb3ca64a961
{elmehdi-rezoug} <{elmehdirezoug100@gmail.com}>
2026-08-05 04:24:22 +0100

    Update template to use CDN for Bootstrap and Font Awesome stylesheets and scripts

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ url('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ url('fontawesome\css\fontawesome.css') }}">
-    <link rel="stylesheet" href="{{ url('fontawesome\css\solid.css') }}">
-    <link rel="stylesheet" href="{{ url('fontawesome\css\brands.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 
 <body class="d-flex flex-column" style="height: 100%">
@@ -25,8 +25,10 @@
 
     @include('partials.footer')
 
-    <script src="js\jquery-3.7.1.min.js"></script>
-    <script src="js\bootstrap.bundle.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
